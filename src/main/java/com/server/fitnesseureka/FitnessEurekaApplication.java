5d2f3029d347f436ffd81f71d8012107cb88e353
Nageswarareddy Chaduvula <dt241115@ssnc-corp.global>
2025-04-16 17:31:49 +0530

    making service as eureka server

--- src/main/java/com/server/fitnesseureka/FitnessEurekaApplication.java
+++ src/main/java/com/server/fitnesseureka/FitnessEurekaApplication.java
@@ -2,8 +2,10 @@ package com.server.fitnesseureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer	// To act this spring boot microservices application as a Eureka server
 public class FitnessEurekaApplication {
 
 	public static void main(String[] args) {

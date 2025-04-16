6e10143e4b2ca115add99979ecdac800178a196d
Nageswarareddy Chaduvula <dt241115@ssnc-corp.global>
2025-04-16 14:47:39 +0530

    initial commit

---
+++ src/main/java/com/server/fitnesseureka/FitnessEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.server.fitnesseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/fitnesseureka/FitnessEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.fitnesseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
